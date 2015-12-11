1df7eee410c0a392771ed2dfff9194eb0dd535bb
Abraham Kazzaz <akazzaz@users.noreply.github.com>
2015-12-11 14:26:58 +1100

    Make post-processing an option
    Make POSTPROCESS a BRANCH of the code and run the post-processing code.

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ cout<<endl<<endl<<"Version 2013 for Integrated DEM simulations"<<endl<<endl;
     cout<<"\tCAPDEM: DEM for capillary interaction"<<endl;
     cout<<"\tTBM: DEM code for fusion blanket TBM systems"<<endl;
     cout<<"\tNORMAL: Standard input"<<endl;
+    cout<<"\tPOSTPROCESS: Post processing output files of simulations"<<endl;//AK mod - Reactivate post-processing
     cin>> BRANCH;
     
     // Boundary type: cubic box / cylidical + periodic / wall
@@ -58,8 +59,16 @@ if(BRANCH=="CREATE")
 	run.config.fprint(run.where_save);//save the final configuration
 }
 
+// AK mod start - Reactivate post-processing
+else if(BRANCH=="POSTPROCESSING")
+{
+	Crun_post_process PPrun;
+	PPrun.init();
+	PPrun.post_process();
+}
+// AK mod end - Reactivate post-processing
 
-if(BRANCH!="CREATE")
+else if(BRANCH!="CREATE")
 	{
         Crun Lrun;
 		
